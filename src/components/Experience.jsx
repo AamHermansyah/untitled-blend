3905aabaf34802c3feb8436426536e4dc1eee538
Aam Hermansyah <74630284+AamHermansyah@users.noreply.github.com>
2022-10-10 09:33:25 +0700

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,7 @@ import { myExperience } from "../static_data"
 import styles from "../styles"
 
 const Experience = () => (
-    <section id="experience" className='flex flex-col justify-center min-h-[100vh] lg:max-h-[700px] relative' >
+    <section id="experience" className='flex flex-col justify-center h-[100vh] lg:h-[650px] relative' >
         <div className="self-center mt-10 mb-10">
             <h1 className={`${styles.heading} text-center`} >
                 My {" "}
